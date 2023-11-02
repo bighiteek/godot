109d340bae9f92f10ae57bbc9dc16b7bbfe23419
tourer224 <149575884+tourer224@users.noreply.github.com>
2023-11-02 13:13:15 +0500

    Delete NHFRNF2/NHFRNF2 directory

--- NHFRNF2/NHFRNF2/Scripts/Sun_script.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-var rotation_speed = 1.0 # Скорость вращения спрайта
-
-func _process(delta):
-	rotate_sprite(delta) # Вызов функции вращения спрайта
-
-func rotate_sprite(delta):
-	rotation += rotation_speed * delta # Изменение угла поворота спрайт
-
